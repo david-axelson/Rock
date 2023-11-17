b41d1c94364ef6483ae49b85afa5e6490d1ff473
jonrob <jonathan@triumph.tech>
2023-11-17 13:13:36 -0700

    - Added obsidian Financial Statement Template List

---
+++ Rock.Blocks/Finance/FinancialStatementTemplateList.cs
@@ -0,0 +1,178 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Obsidian.UI;
+using Rock.Security;
+using Rock.ViewModels.Blocks;
+using Rock.ViewModels.Blocks.Finance.FinancialStatementTemplateList;
+using Rock.Web.Cache;
+
+namespace Rock.Blocks.Finance
+{
+    /// <summary>
+    /// Displays a list of financial statement templates.
+    /// </summary>
+
+    [DisplayName( "Financial Statement Template List" )]
+    [Category( "Finance" )]
+    [Description( "Displays a list of financial statement templates." )]
+    [IconCssClass( "fa fa-list" )]
+    // [SupportedSiteTypes( Model.SiteType.Web )]
+
+    [LinkedPage( "Detail Page",
+        Description = "The page that will show the financial statement template details.",
+        Key = AttributeKey.DetailPage )]
+
+    [Rock.SystemGuid.EntityTypeGuid( "4d735bea-0b50-4232-971e-b477849610bd" )]
+    [Rock.SystemGuid.BlockTypeGuid( "fceb8853-8841-48f6-bfca-f28d9106b617" )]
+    [CustomizedGrid]
+    public class FinancialStatementTemplateList : RockEntityListBlockType<FinancialStatementTemplate>
+    {
+        #region Keys
+
+        private static class AttributeKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        private static class NavigationUrlKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        #endregion Keys
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public override object GetObsidianBlockInitialization()
+        {
+            var box = new ListBlockBox<FinancialStatementTemplateListOptionsBag>();
+            var builder = GetGridBuilder();
+
+            box.IsAddEnabled = GetIsAddEnabled();
+            box.IsDeleteEnabled = BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+            box.ExpectedRowCount = null;
+            box.NavigationUrls = GetBoxNavigationUrls();
+            box.Options = GetBoxOptions();
+            box.GridDefinition = builder.BuildDefinition();
+
+            return box;
+        }
+
+        /// <summary>
+        /// Gets the box options required for the component to render the list.
+        /// </summary>
+        /// <returns>The options that provide additional details to the block.</returns>
+        private FinancialStatementTemplateListOptionsBag GetBoxOptions()
+        {
+            var options = new FinancialStatementTemplateListOptionsBag();
+
+            return options;
+        }
+
+        /// <summary>
+        /// Determines if the add button should be enabled in the grid.
+        /// <summary>
+        /// <returns>A boolean value that indicates if the add button should be enabled.</returns>
+        private bool GetIsAddEnabled()
+        {
+            return BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+        }
+
+        /// <summary>
+        /// Gets the box navigation URLs required for the page to operate.
+        /// </summary>
+        /// <returns>A dictionary of key names and URL values.</returns>
+        private Dictionary<string, string> GetBoxNavigationUrls()
+        {
+            return new Dictionary<string, string>
+            {
+                [NavigationUrlKey.DetailPage] = this.GetLinkedPageUrl( AttributeKey.DetailPage, "StatementTemplateId", "((Key))" )
+            };
+        }
+
+        /// <inheritdoc/>
+        protected override IQueryable<FinancialStatementTemplate> GetListQueryable( RockContext rockContext )
+        {
+            return base.GetListQueryable( rockContext );
+        }
+
+        /// <inheritdoc/>
+        protected override GridBuilder<FinancialStatementTemplate> GetGridBuilder()
+        {
+            return new GridBuilder<FinancialStatementTemplate>()
+                .WithBlock( this )
+                .AddField( "id", a => a.Id )
+                .AddTextField( "idKey", a => a.IdKey )
+                .AddTextField( "name", a => a.Name )
+                .AddTextField( "description", a => a.Description )
+                .AddField( "isActive", a => a.IsActive )
+                .AddField( "isSecurityDisabled", a => !a.IsAuthorized( Authorization.ADMINISTRATE, RequestContext.CurrentPerson ) )
+                .AddAttributeFields( GetGridAttributes() );
+        }
+
+        #endregion
+
+        #region Block Actions
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="key">The identifier of the entity to be deleted.</param>
+        /// <returns>An empty result that indicates if the operation succeeded.</returns>
+        [BlockAction]
+        public BlockActionResult Delete( string key )
+        {
+            using ( var rockContext = new RockContext() )
+            {
+                var entityService = new FinancialStatementTemplateService( rockContext );
+                var entity = entityService.Get( key, !PageCache.Layout.Site.DisablePredictableIds );
+
+                if ( entity == null )
+                {
+                    return ActionBadRequest( $"{FinancialStatementTemplate.FriendlyTypeName} not found." );
+                }
+
+                if ( !BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson ) )
+                {
+                    return ActionBadRequest( $"Not authorized to delete ${FinancialStatementTemplate.FriendlyTypeName}." );
+                }
+
+                if ( !entityService.CanDelete( entity, out var errorMessage ) )
+                {
+                    return ActionBadRequest( errorMessage );
+                }
+
+                entityService.Delete( entity );
+                rockContext.SaveChanges();
+
+                return ActionOk();
+            }
+        }
+
+        #endregion
+    }
+}
